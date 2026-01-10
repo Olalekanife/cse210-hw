ef8de647512483f26ed4b5d410c9b90de12df089
olalekanife <oifeoluwa@byupathway.edu>
2026-01-10 02:15:58 +0100

    #completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -2,16 +2,16 @@ using System;
 
 class Program
 {
-    //Ask the user for their name
+    //Ask the user for their name.
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string first = Console.Readline();
+        string first = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string last = Console.ReadLine();
 
-        Console.Writeline($"Your name is {last}, {first} {last}");
+        Console.WriteLine($"Your name is {last}, {first} {last}");
 
-    }
+    } 
 }
